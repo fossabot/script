6cca370e89425786888bda3850e20097d27c1d26
natthasath <natthasath.sak@gmail.com>
2020-11-18 09:58:22 +0700

    Push

--- oracle-script/count-objects.sh
+++ oracle-script/count-objects.sh
@@ -1,3 +1,3 @@
 SPOOL ../Report/$ORACLE_SID/count_objects.txt
-SELECT COUNT(object_name), owner, object_type FROM dba_objects WHERE owner IN ('MEISPSN','HRIS') GROUP BY owner, object_type ORDER BY owner ;
+SELECT COUNT(object_name), owner, object_type FROM dba_objects WHERE owner IN ('ORCL') GROUP BY owner, object_type ORDER BY owner ;
 SPOOL OFF
