0cf23a1f4071e8a09770838896ac804aba905d6f
natthasath <natthasath.sak@gmail.com>
2020-11-24 14:25:40 +0700

    Push

--- oracle-script/clear-archivelog.sh
+++ oracle-script/clear-archivelog.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-rman target /<< EOF
+for i in PRO DEV
+do
+
+export ORACLE_SID = $i
+rman target / << EOF
 sql 'alter system switch logfile';
 crosscheck archivelog all;
 delete noprompt archivelog until time 'SYSDATE-7';
+EOF
+
+echo $i
+done
