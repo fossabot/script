1ebdd57d8fdca50da9bb1b6fcd9ce949cec245c9
natthasath <natthasath.sak@gmail.com>
2020-12-02 16:08:17 +0700

    Push

--- batch-script/Home-Browser.bat
+++ batch-script/Home-Browser.bat
@@ -1,6 +1,5 @@
 @echo off
-REG ADD "HKCU\SOFTWARE\MICROSOFT\INTERNET EXPLORER\MAIN" /V "START PAGE" /D "http://www.google.com/" /F 
-@echo off
-REG ADD "HKCU\SOFTWARE\MICROSOFT\GOOGLE CHROME\MAIN" /V "START PAGE" /D "http://www.google.com/" /F 
-@echo off
-REG ADD "HKCU\SOFTWARE\MICROSOFT\MOZILLA FIREFOX\MAIN" /V "START PAGE" /D "http://www.google.com/" /F
+
+reg add "HKCU\SOFTWARE\MICROSOFT\INTERNET EXPLORER\MAIN" /V "START PAGE" /D "http://www.google.com/" /F
+reg add "HKCU\SOFTWARE\MICROSOFT\GOOGLE CHROME\MAIN" /V "START PAGE" /D "http://www.google.com/" /F
+reg add "HKCU\SOFTWARE\MICROSOFT\MOZILLA FIREFOX\MAIN" /V "START PAGE" /D "http://www.google.com/" /F
--- batch-script/Resize-FileExplorer.bat
+++ batch-script/Resize-FileExplorer.bat
@@ -1,18 +1,13 @@
 @echo off
 
-# To reset all folders to default folder views
-Reg Delete "HKCU\Software\Microsoft\Windows\Shell\BagMRU" /F
-Reg Delete "HKCU\Software\Microsoft\Windows\Shell\Bags" /F
+reg delete "HKCU\Software\Microsoft\Windows\Shell\BagMRU" /F
+reg delete "HKCU\Software\Microsoft\Windows\Shell\Bags" /F
+reg delete "HKCU\Software\Microsoft\Windows\ShellNoRoam\Bags" /F
+reg delete "HKCU\Software\Microsoft\Windows\ShellNoRoam\BagMRU" /F
+reg delete "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\BagMRU" /F
+reg delete "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\Bags" /F
+reg delete "HKCU\Software\Classes\Wow6432Node\Local Settings\Software\Microsoft\Windows\Shell\Bags" /F
+reg delete "HKCU\Software\Classes\Wow6432Node\Local Settings\Software\Microsoft\Windows\Shell\BagMRU" /F
 
-Reg Delete "HKCU\Software\Microsoft\Windows\ShellNoRoam\Bags" /F
-Reg Delete "HKCU\Software\Microsoft\Windows\ShellNoRoam\BagMRU" /F
-
-Reg Delete "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\BagMRU" /F
-Reg Delete "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\Bags" /F
-
-Reg Delete "HKCU\Software\Classes\Wow6432Node\Local Settings\Software\Microsoft\Windows\Shell\Bags" /F
-Reg Delete "HKCU\Software\Classes\Wow6432Node\Local Settings\Software\Microsoft\Windows\Shell\BagMRU" /F
-
-# To kill and restart explorer
 taskkill /f /im explorer.exe
 start explorer.exe
